c0abb9c486bf0eb699769727f3989d519c7c7c44
sahla <147273668+sahlaks@users.noreply.github.com>
2023-10-26 15:23:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('#menu');
+console.log(menu);
+const sidebar = document.querySelector('.sidebar');
+console.log(sidebar);
+
+menu.addEventListener('click', function () {
+  sidebar.classList.toggle('show-sidebar');
+});
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
